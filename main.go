cab5db22935216d3631aa833462a9325d1294b8c
maharajafz03 <maharajafz1999@gmail.com>
2024-10-17 17:04:43 +0530

    [golang]

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello_world") // Use fmt.Println (note the capitalization)
+
+	sub()
+}
+
+
+func sub() {
+	fmt.Println("welcome_bacl")
+}
